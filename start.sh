05d2ec06a96417ecebf3bd83d079c446e52c478a
Si Beaumont <simon.beaumont@citrix.com>
2014-09-08 13:42:55 +0100

    chkconfig --add the things
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- start.sh
+++ start.sh
@@ -2,18 +2,28 @@
 
 rm /etc/sysconfig/network-scripts/ifcfg-eth0
 
+chkconfig --add forkexecd
 service forkexecd start
+chkconfig --add xcp-networkd
 service xcp-networkd start
+chkconfig --add genptoken
 service genptoken start
+chkconfig --add squeezed
 service squeezed start
+chkconfig --add xcp-rrdd
 service xcp-rrdd start
+chkconfig --add xenopsd-xc
 service xenopsd-xc start
+chkconfig --add xapi
 service xapi start
 
 sleep 30
 
+chkconfig --add xcp-rrdd-plugins
 service xcp-rrdd-plugins start
+chkconfig --add firstboot
 service firstboot start
+chkconfig --add perfmon
 service perfmon start
 
 . /etc/xensource-inventory
