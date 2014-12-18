61dd3c47944d0255ec554698bae337d29fd8ab05
Jon Ludlam <jonathan.ludlam@citrix.com>
2014-12-18 11:22:37 +0000

    Chkconfig everything on too, to survive a reboot

--- start.sh
+++ start.sh
@@ -3,18 +3,27 @@
 rm /etc/sysconfig/network-scripts/ifcfg-eth0
 
 service forkexecd start
+chkconfig forkexecd on
 service xcp-networkd start
+chkconfig xcp-networkd on
 service genptoken start
+chkconfig genptoken on
 service squeezed start
+chkconfig squeezed on
 service xcp-rrdd start
+chkconfig xcp-rrdd on
 service xenopsd-xc start
+chkconfig xenopsd-xc on
 service xapi start
+chkconfig xapi on
 
 sleep 30
 
 service xcp-rrdd-plugins start
+chkconfig xcp-rrdd-plugins on
 service firstboot start
 service perfmon start
+chkconfig perfmon on
 
 . /etc/xensource-inventory
 xe pif-scan host-uuid=${INSTALLATION_UUID}
