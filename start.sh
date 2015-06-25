8a31241bd68db60ab66e83f69f6cae4ea4842990
Jon Ludlam <jonathan.ludlam@citrix.com>
2015-06-25 17:12:06 +0100

    Remove perfmon from start.sh
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- start.sh
+++ start.sh
@@ -22,8 +22,8 @@ sleep 30
 service xcp-rrdd-plugins start
 chkconfig xcp-rrdd-plugins on
 service firstboot start
-service perfmon start
-chkconfig perfmon on
+#service perfmon start
+#chkconfig perfmon on
 
 . /etc/xensource-inventory
 xe pif-scan host-uuid=${INSTALLATION_UUID}
